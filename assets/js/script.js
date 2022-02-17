7b71f3b610ca48493177a84bf91b7838936a851f
Boris Veselov <borisveselov@yahoo.com>
2022-02-16 20:28:10 -0500

    form reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,12 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if (!taskNameInput || !taskTypeInput) {
+        
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formEl.reset();
 // package up data as an object
   var taskDataObj = {
     name: taskNameInput,
@@ -14,7 +20,7 @@ var taskFormHandler = function(event) {
 
   // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
-}
+};
     
 var createTaskEl = function(taskDataObj) {
 // create list item
@@ -25,7 +31,6 @@ listItemEl.className = "task-item";
 var taskInfoEl = document.createElement("div");
 taskInfoEl.className = "task-info";
 taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-
 listItemEl.appendChild(taskInfoEl);
 
 // add entire list item to list
